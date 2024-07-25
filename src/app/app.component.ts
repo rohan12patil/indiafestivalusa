523d464e43afd29181e078f5440a5a8e5119cccd
Rohan Patil <rohan.patil@pfizer.com>
2024-07-25 18:03:35 +0100

    NG 18 fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'indiafestivalusa';
